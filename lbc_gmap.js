ab9daaf6eff54395198017a29d68c1549249f498
dvdn <dvdngrllo@gmail.com>
2017-01-09 19:30:19 +0100

    all partedcities and no map repetition

--- lbc_gmap.js
+++ lbc_gmap.js
@@ -1,7 +1,13 @@
-var locationData = "", gmapUrl = "https://maps.google.fr/maps?q=france+", zoom = 8, fullgmapUrl = "";
+var partedCities=['paris','lyon', 'marseille'], locationData = "", gmapUrl = "https://maps.google.fr/maps?q=france+", zoom = 7, fullgmapUrl = "";
 locationData = $(".line_city .value").text().toLowerCase();
-if (locationData.indexOf('paris')> -1) {
+
+for each (var city in partedCities) {  
+  if (locationData.indexOf(city) > -1) {
     zoom = 12
+  }
 }
-fullgmapUrl = gmapUrl + locationData + "&z=" + zoom;
-$(".line_city").prepend('<div width="100%" height="320px" align="right"><iframe width="60%" height="320px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="' + fullgmapUrl + '&amp;iwloc=&amp;output=embed"></iframe></div>');
+
+if ($("#gmap-insert").length == 0) {
+  fullgmapUrl = gmapUrl + locationData + "&z=" + zoom;
+  $(".line_city").prepend('<div id="gmap-insert" width="100%" height="320px" align="right"><iframe width="60%" height="320px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="' + fullgmapUrl + '&amp;iwloc=&amp;output=embed"></iframe></div>');
+}
